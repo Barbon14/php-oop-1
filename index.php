3c5170a3f78e7e45a33f2f827a0254b46216ad26
BarbonFisso <barbieri.mat@hotmail.it>
2022-01-17 14:15:48 +0100

    Add: methods to Film

--- index.php
+++ index.php
@@ -2,10 +2,32 @@
 
 class Film {
 
-    public $titel;
+    public $title;
     public $subtitle;
     public $director;
 
+    public function __construct($title) {
+        
+        $this -> title = $title;
+    }
+
+    public function getFullTitle() {
+
+        if ($this -> subtitle != null) {
+            return $this -> title . " " . $this -> subtitle;
+        } else {
+            return $this -> title;
+        }
+    }
+
+    public function __toString() {
+
+        if ($this -> director != null) {
+            return $this -> getFullTitle() . " | " . $this-> director;
+        } else {
+            return $this->getFullTitle() . " | ???";
+        }
+    }
 }
 
 ?>
