38bc3bf876c3d3059e92ee8c6744778def1155cf
BarbonFisso <barbieri.mat@hotmail.it>
2022-01-17 14:22:11 +0100

    Add: new instances

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ class Film {
     public function getFullTitle() {
 
         if ($this -> subtitle != null) {
-            return $this -> title . " " . $this -> subtitle;
+            return $this -> title . ": " . $this -> subtitle;
         } else {
             return $this -> title;
         }
@@ -29,5 +29,18 @@ class Film {
         }
     }
 }
+
+$f1 = new Film("Matrix");
+
+$f2 = new Film("Fantozzi 2");
+$f2 -> subtitle = "il ritorno di Fantozzi";
+
+$f3 = new Film("Peter Pan");
+$f3 -> subtitle = "il ritorno all'isola che non c'è";
+$f3 -> director = "Robin Budd";
+
+echo $f1 . "<br>";
+echo $f2 . "<br>";
+echo $f3 . "<br>";
 
 ?>
